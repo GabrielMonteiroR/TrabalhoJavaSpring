79dd1d0a6414fb082b2c50199466997863ef5ed6
Gabriel <gmonteiro810@gmail.com>
2024-11-24 17:25:59 -0300

    Creating aluno entity

---
+++ src/main/java/university/AlunoEntity.java
@@ -0,0 +1,53 @@
+package university;
+
+public class AlunoEntity {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String matricula;
+    private String data_nascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+}
+
+
+
