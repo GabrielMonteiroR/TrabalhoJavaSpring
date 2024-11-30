9843e38c878379d05ff0b17597abece0fd52ff88
Gabriel <gmonteiro810@gmail.com>
2024-11-30 20:50:28 -0300

    finishing aluno crud

---
+++ src/main/java/university/aluno/dto/AlunoCreateDTO.java
@@ -0,0 +1,3 @@
+package university.aluno.dto;
+
+public record AlunoCreateDTO(String nome, String email, String matricula) {}
---
+++ src/main/java/university/aluno/dto/AlunoListDTO.java
@@ -0,0 +1,5 @@
+package university.aluno.dto;
+
+
+public record AlunoListDTO(String nome, String email, String matricula) {}
+
