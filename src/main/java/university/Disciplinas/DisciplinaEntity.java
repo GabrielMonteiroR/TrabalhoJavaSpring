addd83f20d7e47ae5b3e886e8a92b52b9c601024
Gabriel <gmonteiro810@gmail.com>
2024-12-01 17:58:39 -0300

    Adding DisciplinaEntity

---
+++ src/main/java/university/Disciplinas/DisciplinaEntity.java
@@ -0,0 +1,69 @@
+package university.Disciplinas;
+
+import jakarta.persistence.*;
+import university.Cursos.CursoEntity;
+import university.Professor.ProfessorEntity;
+
+@Entity
+@Table(name = "disciplinas")
+public class DisciplinaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id", nullable = false)
+    private CursoEntity curso;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id", nullable = false)
+    private ProfessorEntity professor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public CursoEntity getCurso() {
+        return curso;
+    }
+
+    public void setCurso(CursoEntity curso) {
+        this.curso = curso;
+    }
+
+    public ProfessorEntity getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(ProfessorEntity professor) {
+        this.professor = professor;
+    }
+}
+
--- src/main/java/university/Turmas/TurmaEntity.java
+++ src/main/java/university/Turmas/TurmaEntity.java
@@ -21,6 +21,36 @@ public class TurmaEntity {
     @JoinColumn(name = "curso_id", nullable = false)
     private CursoEntity curso;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(Integer semestre) {
+        this.semestre = semestre;
+    }
+
+    public CursoEntity getCurso() {
+        return curso;
+    }
+
+    public void setCurso(CursoEntity curso) {
+        this.curso = curso;
+    }
 }
 
