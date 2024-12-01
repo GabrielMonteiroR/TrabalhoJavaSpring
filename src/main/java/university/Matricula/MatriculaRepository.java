d33e8cfdf7cb242e1b9f7f381c0682a0ecbdd742
Gabriel <gmonteiro810@gmail.com>
2024-12-01 18:05:25 -0300

    Adding MatriculaRepository

---
+++ src/main/java/university/Matricula/MatriculaRepository.java
@@ -0,0 +1,5 @@
+package university.Matricula;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<MatriculaEntity, Integer> {}
