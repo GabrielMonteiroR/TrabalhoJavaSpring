4604463c0ac13348d146c7ec2cbd0de91a544b96
Gabriel <gmonteiro810@gmail.com>
2024-12-01 17:53:19 -0300

    Adding Cursos Entity

---
+++ src/main/java/university/Cursos/CursoEntity.java
@@ -0,0 +1,51 @@
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cursos")
+public class CursoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String codigo;
+
+    @Column(nullable = false)
+    private Integer cargaHoraria;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
