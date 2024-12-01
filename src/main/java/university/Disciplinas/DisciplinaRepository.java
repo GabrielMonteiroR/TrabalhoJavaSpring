b049da92fe8ff931bec9cdc07cd6752cfc53c9e0
Gabriel <gmonteiro810@gmail.com>
2024-12-01 18:04:39 -0300

    Adding DisciplinaRepository

---
+++ src/main/java/university/Disciplinas/DisciplinaRepository.java
@@ -0,0 +1,5 @@
+package university.Disciplinas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplinaRepository extends JpaRepository<DisciplinaEntity, Integer> {}
