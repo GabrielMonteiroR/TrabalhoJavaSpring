13b5910aeef38b2dbef15eed281096088805c903
Gabriel <gmonteiro810@gmail.com>
2024-12-01 18:02:46 -0300

    Adding ProfessorRepository

---
+++ src/main/java/university/Professor/ProfessorRepository.java
@@ -0,0 +1,5 @@
+package university.Professor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<ProfessorEntity, Integer> {}
